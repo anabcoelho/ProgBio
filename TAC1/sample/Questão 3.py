3847f710bcdec73881cbb2554e9b1e03c5ead69f
anabcoelho <70607969+anabcoelho@users.noreply.github.com>
2020-09-12 12:15:25 -0300

    Tudo certo

--- TAC1/sample/Questão 3.py
+++ TAC1/sample/Questão 3.py
@@ -1,6 +1,7 @@
-import csv
-with open("C:\\Users\\anabe\\PycharmProjects\\CFB017\\TAC1\\dados\\species.csv", newline='') as f:
-    reader = csv.reader(f)
+import csv # importar a biblioteca csv
+#Abrindo ariquivo na variável f.
+with open("C:\\Users\\anabe\\PycharmProjects\\CFB017\\TAC1\\dados\\species.csv") as f:
+    reader = csv.reader(f) #leitura do arquivo CSV
     for row in reader:
-        if row[3].upper().rstrip() == "BIRD":
+         if row[3].upper().rstrip() == "BIRD": #se na coluna 3 tem bird (escrito de qualquer maneira)
             print (row)
--- TAC1/sample/questão 1.py
+++ TAC1/sample/questão 1.py
@@ -1,13 +1,19 @@
+# Objetivo: Ler arquivo FASTA com apenas um gene e sua sequência. E imprimir o gene e a sequência.
+# Variáveis: Entrada: Arquivoin
+#            Saída: gene, sequencia
+#            De trabalho: cabecalho e linha
+
 Arquivoin = open("C:\\Users\\anabe\\PycharmProjects\\CFB017\\TAC1\\dados"
                   "\\TcCLB.506717.80_mRNA-p1.fasta")
 
-cabecalho = Arquivoin.readline()[1:-1]
-gene=cabecalho[0:15]
-sequencia = ""
-for linha in Arquivoin:
-    sequencia += linha.replace('\n','')
+cabecalho = Arquivoin.readline()[1:-1] #ler a 1a linha do arquivo
+gene=cabecalho[0:15] #extraindo o gene do cabeçalho
+sequencia = "" #abertura da string sequencia
 
-print ("%s\t%s"% (gene,sequencia))
+for linha in Arquivoin:
+    sequencia += linha.replace('\n','') #append das linhas da sequência
+
+print ("%s\t%s"% (gene,sequencia)) #
 Arquivoin.close()
 
 
