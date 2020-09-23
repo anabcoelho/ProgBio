84b15e4a8dbe5f782ba44656835c4b718617343d
anabcoelho <70607969+anabcoelho@users.noreply.github.com>
2020-09-23 15:06:20 -0300

    Lista de exercícios

--- Lista1/sample/Exercício.py
+++ Lista1/sample/Exercício.py
@@ -7,8 +7,25 @@ print (' Máximo de mortos %d' % df ['TB deaths'].max ())
 print (' Mínimo de mortos %d' % df ['TB deaths'].min ())
 print (' Média de mortos %d' % df ['TB deaths'].mean ())
 print (' Mediana de mortos %d' % df ['TB deaths'].median ())
-print ('Ordenando de maneira crescente: \n %s ' %df.sort_values('TB deaths'))
-Norm = df['TB deaths']/
-print(Norm)
-data_normalizado =
-print(data_normalizado)
+dataord= df.sort_values('TB deaths')
+print ('Ordenando de maneira crescente: \n %s ' %dataord)
+datanorm=dataord
+datanorm['TB deaths'] = datanorm['TB deaths']/100000 # nomalizando
+
+print( 'Ordenado e Normalizado \n %s' %datanorm)
+# BRICS
+#só consegui dessa forma
+#crei um dicionário (dic) com o b,r,i,c,s e mandei pd.dataframe(dic)
+# O dataframe tava aparecendo como vazio, apenas com as colunas
+b=df.loc[df['Country'] == "Brazil"]
+soma= int(b['TB deaths'])
+r=df.loc[df['Country'] == "Russian Federation"]
+soma= int(r['TB deaths'])
+i=df.loc[df['Country'] == "India"]
+soma= soma+ int(i['TB deaths'])
+c=df.loc[df['Country'] == "China"]
+soma= soma+ int(c['TB deaths'])
+s=df.loc[df['Country'] == "South Africa"]
+soma= soma+ int(s['TB deaths'])
+
+print(soma)
