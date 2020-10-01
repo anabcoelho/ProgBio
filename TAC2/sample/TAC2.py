887e79171fbbc1f2c6f8889a67f9c3e07cc09048
anabcoelho <70607969+anabcoelho@users.noreply.github.com>
2020-10-01 09:21:47 -0300

    tudo ok

--- TAC2/sample/TAC2.py
+++ TAC2/sample/TAC2.py
@@ -1,13 +1,17 @@
+
 import sys
-l= sys.argv # Entrada arquivo m b
+import pandas as pd
+
+l= sys.argv # Entrada: arquivo m b
 excel= l[1]
 m= float(l[2])
 b= float (l[3])
-import pandas as pd
-with pd.ExcelFile(excel) as xlsx:
+
+
+with pd.ExcelFile(excel) as xlsx: #leitura arquivo
     df = pd.read_excel(xlsx)
 
-
+#Normalizando...
 fatornorm = 10** (df['CT']-b)/m
 
 data_q = {
@@ -15,10 +19,15 @@ data_q = {
     'Stage' : df['Stage'],
     'Quantity': fatornorm,
 }
+
 df_q = pd.DataFrame(data_q, columns=['Target_Name','Stage','Quantity'])
+
 Qt=df_q['Quantity']
+
 df_final= df
+
 df['Quantity'] = Qt
 
 print (df_final)
-df.to_csv (r'..\\dados\\saida.csv', index = False, header=True)
+
+df.to_csv (r'..\\dados\\saida.csv', index = False, header=True) #saída para CSV
